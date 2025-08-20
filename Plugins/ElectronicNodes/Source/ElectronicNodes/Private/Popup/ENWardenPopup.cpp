0b2f6f5aecd57b87d0d04151e2fca3fe85bcee39
njit-lx <2200264290@qq.com>
2025-08-20 22:56:27 +0800

    Normalize line endings

---
+++ Plugins/ElectronicNodes/Source/ElectronicNodes/Private/Popup/ENWardenPopup.cpp
@@ -0,0 +1,35 @@
+﻿#include "ENWardenPopup.h"
+#include "IPluginWardenModule.h"
+
+bool ENWardenPopup::bHasBeenChecked = false;
+
+void ENWardenPopup::Register()
+{
+	if (bHasBeenChecked)
+	{
+		return;
+	}
+	
+	bHasBeenChecked = true;
+
+	FModuleManager::Get().LoadModuleChecked<IPluginWardenModule>("PluginWarden");
+
+	if (IPluginWardenModule::IsAvailable())
+	{
+		CheckEntitlement();
+	}
+}
+
+void ENWardenPopup::CheckEntitlement()
+{
+	IPluginWardenModule::Get().CheckEntitlementForPlugin(
+		FText::FromString("Electronic Nodes"),
+		TEXT("5cb2a394d0c04e73891762be4cbd7216"),
+		TEXT(""),
+		FText(),
+		IPluginWardenModule::EUnauthorizedErrorHandling::ShowMessageOpenStore,
+		[&]()
+		{
+		}
+	);
+}
